8846279781e2bb27eeb7057d4df2ad8dce49b7d7
Rafael Santos <rafoliael@gmail.com>
2024-12-12 19:15:25 -0300

    setting-up

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/santosraff/rss-agregator
 
 go 1.23.4
+
+require github.com/joho/godotenv v1.5.1
--- main.go
+++ main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
 
 func main () {
 	fmt.Println("hello world")
+
+	godotenv.Load(".env")
+
+	portString := os.Getenv("PORT")
+	if portString == "" {
+		log.Fatal("PORT not found in the enviroment")
+	}
+	fmt.Println("Port:", portString)
 }
